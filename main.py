2b200a7c82cce34cea40b31b2b13ea8c1c948dd4
johan2898 <76543042+johan2898@users.noreply.github.com>
2023-04-30 18:45:40 -0400

    Update main.py
    Migrating from Kivy to KivyMD

--- main.py
+++ main.py
@@ -1,58 +1,72 @@
-from kivy.app import App
-from kivy.properties import StringProperty, BooleanProperty
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
-from kivy.uix.widget import Widget
 from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager, Screen
 from kivymd.app import MDApp
-from kivymd.uix.pickers import MDDatePicker
-from kivymd.uix.pickers import MDTimePicker
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.floatlayout import FloatLayout
+from kivy.properties import ObjectProperty
+from kivymd.uix.pickers import MDTimePicker, MDDatePicker
+import sqlite3
 
 
-class WindowManager(ScreenManager):
-    pass
+class MainLayout(FloatLayout):
+    screen_manager = ObjectProperty(None)
 
 
-class AddTask(Screen):
-    pass
+class MainApp(MDApp):
+    def build(self):
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "Cyan"
+        # create/connet to db
+        conn = sqlite3.connect("tasks_db.db")
+        # cursor
+        c = conn.cursor()
+        c.execute("""CREATE TABLE tasks(schedule text, tittle text, duedate text)
+        """)
+        conn.commit()
+        conn.close()
+        return Builder.load_file("manager.kv")
 
+    def clear(self):
+        self.root.ids.user.text = ""
+        self.root.ids.password.text = ""
 
-class Tasks(Screen):
+    def on_save(self, instance, value, date_range):
+        self.root.ids.duedate.text = str(value)
+
+    def on_cancel(self, instance, value):
+        self.root.ids.duedate.text = ""
 
     def date_picker(self):
-        time_dialog = MDTimePicker()
-        time_dialog.open()
         date_dialog = MDDatePicker()
+        date_dialog.bind(on_save=self.on_save, on_cancel=self.on_cancel)
         date_dialog.open()
 
-    pass
+    def submit(self):
+        # create/connet to db
+        conn = sqlite3.connect("tasks_db.db")
+        # cursor
+        c = conn.cursor()
+        c.execute("INSERT INTO tasks VALUES(:schedule, :tittle, :duedate)", {
+            'schedule': self.root.ids.datainsert.name,
+            'tittle': self.root.ids.duedate.text,
+            'duedate': self.root.ids.duedate.text,
+        })
+        conn.commit()
+        conn.close()
+        self.root.ids.duedate.text = ''
+        self.root.ids.taskdecrip.text = ''
+
+    def show_record(self):
+        conn = sqlite3.connect("tasks_db.db")
+        # cursor
+        c = conn.cursor()
+        c.execute("SELECT schedule FROM tasks")
+        records = c.fetchall()
+        word = ''
+        for record in records:
+            word = f"{word}\n {record}"
+            self.root.ids.welcome_label.text = f'{word}'
+        conn.commit()
+        conn.close()
 
 
-class BoxLayout1(Screen):
-    my_text = StringProperty("0 tasks")
-    task_count = 0
-    empty_task = BooleanProperty(True)
-
-    def clicked(self):
-        self.task_count += 1
-        self.my_text = f" Task: {self.task_count}"
-        self.empty_task = False
-
-    pass
-
-
-class ScheduleView(Screen):
-    pass
-
-
-class MainWidget(Widget):
-    pass
-
-
-class TheLabApp(MDApp):
-    pass
-
-
-TheLabApp().run()
-
+MainApp().run()
